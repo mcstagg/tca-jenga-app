f8d7d7a5ab6d7b874b4ca8612096364da25d3a81
mcstagg <mcstagg@madisoncollege.edu>
2021-02-23 21:29:30 -0600

    inline styles eliminated
    moved to App.css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import jenga from './Jenga.png';
+import jenga from './tower.PNG';
 import './materialize.css';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <div className="row blue darken-1" style={{margin: 0 + 'em', padding: .5 + 'em'}}>
-      <div className="col s1">1</div>
-      <div className="col s1">2</div>
-      <div className="col s1">3</div>
-      <div className="col s1">4</div>
-      <div className="col s1">5</div>
-      <div className="col s1">6</div>
-      <div className="col s1">7</div>
-      <div className="col s1">8</div>
-      <div className="col s1">9</div>
-      <div className="col s1">10</div>
-      <div className="col s1">11</div>
-      <div className="col s1">12</div>
+    <>
+      <div className="row blue darken-1 top-bar">
+        <div className="col s1">1</div>
+        <div className="col s1">2</div>
+        <div className="col s1">3</div>
+        <div className="col s1">4</div>
+        <div className="col s1">5</div>
+        <div className="col s1">6</div>
+        <div className="col s1">7</div>
+        <div className="col s1">8</div>
+        <div className="col s1">9</div>
+        <div className="col s1">10</div>
+        <div className="col s1">11</div>
+        <div className="col s1">12</div>
       </div>
-      <div className="row deep-orange darken-1" style={{margin: 1 + 'em'}}>
-      <img src={jenga} alt="jenga" className="col s3" />
-      <h1 className="col s9">Jenga Companion</h1>
+
+      <div className="row deep-orange darken-1 header">
+        <img src={jenga} alt="jenga" className="col s3 header-towers" />
+        <h1 className="col s6 white-text header-text">Jenga <br/> Companion</h1>
+        <img src={jenga} alt="jenga" className="col s3 header-towers" />
       </div>
-      <div className="row blue darken-1" style={{margin: 0 + 'em', padding: 1 + 'em'}}>
-      <h4 className="col s6" style={{margin: 0 + 'em', padding: 0 + 'em', textAlign: 'center'}}>Leaderboard</h4>
-      <a className="waves-effect waves-light btn col 2 offset-s2"><i className="material-icons right">jenga!</i>Play</a>
+
+      <div className="row blue darken-1 leaderboard-header">
+        <h4 className="col s6 lb-header-text">Leaderboard</h4>
+        <a className="waves-effect waves-light btn col 2 offset-s2"><i className="material-icons right">jenga!</i>Play</a>
       </div>
+
       <table>
         <thead>
           <tr>
@@ -153,11 +158,12 @@ function App() {
           </tr>
         </tbody>
       </table>
-      <div className="row blue darken-1" style={{margin: 0 + 'em', padding: 1 + 'em'}}>
-      <h4 className="col s6" style={{margin: 0 + 'em', padding: 0 + 'em', textAlign: 'center'}}>Ready to Start?</h4>
-      <a className="waves-effect waves-light btn col 2 offset-s2"><i className="material-icons right">jenga!</i>Play</a>
+
+      <div className="row blue darken-1 leaderboard-header">
+        <h4 className="col s6 lb-header-text">Ready to Start?</h4>
+        <a className="waves-effect waves-light btn col 2 offset-s2"><i className="material-icons right">jenga!</i>Play</a>
       </div>
-    </div>
+    </>
   );
 }
 
