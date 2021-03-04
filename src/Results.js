ba4e1302bb9fdccda1e076f1b6f0ca19e4e44a08
mcstagg <mcstagg@madisoncollege.edu>
2021-03-04 15:43:55 -0600

    results structure setup for post game

--- src/Results.js
+++ src/Results.js
@@ -3,12 +3,132 @@ import React from 'react';
 const Results = (props) => {
   
   return (
-    <div className="row gameplay">
-      <h2>Winner!</h2>
-      <h3>Player 1</h3>
-      <h2>Loser!</h2>
-      <h3>Player 2</h3>
+    <>
+    <div className="p-2 mt-1 card-panel light-blue lighten-1 center">
+      <h2 className="white-text">Winner!  Mike</h2>
     </div>
+    <div className="row">
+      <h5 className="col s6">Mike's Stats</h5>
+    </div>
+    <table className="">
+      <tr className="">
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+    </table>
+
+    <div className="p-2 card-panel purple lighten-1 center">
+      <h2 className="white-text">Loser!  Travis</h2>
+    </div>
+    <div className="row">
+      <h5 className="col s6">Travis's Stats</h5>
+    </div>
+    <table className="">
+      <tr className="">
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+    </table>
+
+    <div className="p-2 card-panel deep-orange darken-1 center">
+      <h2 className="white-text">JENGA!</h2>
+    </div>
+
+    <div className="row">
+      <h5 className="col s6">Game Stats</h5>
+    </div>
+
+    <table className="">
+      <tr className="">
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+        <td>
+          <h6>Total Game Time : 01:22:14</h6>
+        </td>
+      </tr>
+    </table>
+    </>
   )    
 }
 
