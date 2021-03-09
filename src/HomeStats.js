ae6fff883e2c03b6f015404648c8c7ac3a1757e8
mcstagg <mcstagg@madisoncollege.edu>
2021-03-09 14:32:52 -0600

    home-stats mobile first

--- src/HomeStats.js
+++ src/HomeStats.js
@@ -6,9 +6,9 @@ const HomeStats = () => {
 
   return (
     <>
-      <Leader h4text="Leaderboard" h4textcol="col s4" moveButton={<a href="/gameSetup" className={`waves-effect waves-light btn-large col s6 m3 offset-s3-m5 valign-wrapper purple lighten-1 buttons`}>PLAY JENGA!<i className="material-icons"></i></a>} />
+      <Leader h4text="Leaderboard" h4textcol="col s6 m9" atts="" moveButton={<a href="/gameSetup" className={`waves-effect waves-light btn-large col s6 m3 valign-wrapper purple lighten-1 buttons`}>PLAY JENGA!<i className="material-icons"></i></a>} />
       <Table />
-      <Leader h4text="Ready to Start?" h4textcol="col s9" atts="" moveButton={<a href="/gameSetup" className={`waves-effect waves-light btn-large col s6 valign-wrapper purple lighten-1 buttons`}>PLAY JENGA!<i className="material-icons"></i></a>}/>
+      <Leader h4text="Ready to Start?" h4textcol="col s9 m7 offset-m2" atts="" moveButton={<a href="/gameSetup" className={`waves-effect waves-light btn-large col s6 m3 valign-wrapper purple lighten-1 buttons`}>PLAY JENGA!<i className="material-icons"></i></a>}/>
     </>
   )    
 }   
--- src/Leader.js
+++ src/Leader.js
@@ -5,7 +5,7 @@ const Leader = (props) => {
   
   return (
     <div className={`row blue darken-1 lb-header valign-wrapper`}>
-      <h4 className={`${props.h4textcol} lb-header-text m-0 p-0 ${props.atts}`}>{props.h4text}</h4>
+      <h4 className={`${props.h4textcol} lb-header-text ${props.atts}`}>{props.h4text}</h4>
       {props.moveButton}
       {props.jengaButton}
       {props.quitButton}
