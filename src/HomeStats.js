17931766a5be546d8c98323bbd2c66108553b750
mcstagg <mcstagg@madisoncollege.edu>
2021-03-08 20:16:25 -0600

    table size adjusted for mobile

--- src/HomeStats.js
+++ src/HomeStats.js
@@ -8,7 +8,7 @@ const HomeStats = () => {
     <>
       <Leader h4text="Leaderboard" h4textcol="col s4" moveButton={<a href="/gameSetup" className={`waves-effect waves-light btn-large col s6 m3 offset-s3-m5 valign-wrapper purple lighten-1 buttons`}>PLAY JENGA!<i className="material-icons"></i></a>} />
       <Table />
-      <Leader h4text="Ready to Start?" h4textcol="col s7" atts="pl-2" moveButton={<a href="/gameSetup" className={`waves-effect waves-light btn-large col s3 offset-s2 valign-wrapper purple lighten-1 buttons`}>PLAY JENGA!<i className="material-icons"></i></a>}/>
+      <Leader h4text="Ready to Start?" h4textcol="col s9" atts="" moveButton={<a href="/gameSetup" className={`waves-effect waves-light btn-large col s6 valign-wrapper purple lighten-1 buttons`}>PLAY JENGA!<i className="material-icons"></i></a>}/>
     </>
   )    
 }   
