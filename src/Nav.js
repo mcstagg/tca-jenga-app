db810d4ba02b7df73f0eaa36caa0c9bffd35ba64
mcstagg <mcstagg@madisoncollege.edu>
2021-03-13 09:43:59 -0600

    nav hover adjustments

--- src/Nav.js
+++ src/Nav.js
@@ -8,16 +8,16 @@ const Nav = (props) => {
   return (
       <div className="">
       <div className=" row nav blue darken-1">
-        <h6 className="col s3 center">
+        <h6 className="col s3 center nav-h6">
            <Link to='/' className="nav-links">Home Stats</Link>   
          </h6>
-         <h6 className="col s3">
+         <h6 className="col s3 nav-h6">
            <Link to='/gameSetup' className="nav-links">Game Setup</Link>   
          </h6>
-         <h6 className="col s3">
+         <h6 className="col s3 nav-h6">
            <Link to='/gameplay' className="nav-links">Game Play</Link>   
          </h6>
-         <h6 className="col s3">
+         <h6 className="col s3 nav-h6">
            <Link to='/postGame' className="nav-links">Post Game</Link>   
          </h6>
       </div>
