22b8682f0d9190954fe9755daa685d703c3ddf5d
mcstagg <mcstagg@madisoncollege.edu>
2021-03-17 14:35:06 -0500

    banner component responsive
    finished up with basic responsiveness for the banner component

--- src/Banner.js
+++ src/Banner.js
@@ -5,9 +5,9 @@ const Banner = () => {
   
   return (
     <div className="row deep-orange darken-1 banner valign-wrapper">
-      <img src={jenga} alt="jenga" className="col s5 m3 l2 banner-towers p-0" />
-      <h1 className="col s7 m6 l8 white-text banner-text">Jenga <br/> Companion</h1>
-      <img src={jenga} alt="jenga" className="col m3 l2 banner-towers p-0 hide-on-small-only" />
+      <img src={jenga} alt="jenga" className="col s5 m3 banner-towers tower1 p-0" />
+      <h1 className="col s7 m6 white-text banner-text">Jenga <br/> Companion</h1>
+      <img src={jenga} alt="jenga" className="col m3 banner-towers p-0 tower2" />
     </div>
   )    
 }
